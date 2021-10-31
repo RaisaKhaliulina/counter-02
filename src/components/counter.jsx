7d9dc3ff344a4cbe97fb4c2a2d3a7d22b7555827
raisa khaliulina <evadar31@gmail.com>
2021-10-31 21:17:04 +0300

    second commit for lesson 3 practic task 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 
-const Counter = (props) => {
+const Counter = ({id, value, name, onIncrement,onDecrement, onDelete}) => {
 
     const formValue = () => {
-        return props.value ===0 ? "Ноль" : props.value
+        return value ===0 ? "Ноль" : value
     }
 
     const getBadgeClasses = () => {
     let classes = "badge m-2 bg-"
-    classes +=props.value === 0 ? "primary":"danger"
+    classes +=value === 0 ? "primary":"danger"
     return classes
     }
     
     return ( 
       <div>
-        <h4>{props.name}</h4>
+        <h4>{name}</h4>
         <span className={getBadgeClasses()}>{formValue()}</span> 
         <button 
-          onClick={() => props.onIncrement(props.id)}
+          onClick={() => onIncrement(id)}
           className="btn btn-secondary btn-sm m-2">
           Increment
         </button>
         <button
-          onClick={() => props.onDecrement(props.id)}
+          onClick={() => onDecrement(id)}
           className="btn btn-secondary  btn-sm m-2">
           Decrement    
         </button>
-        <button className="btn btn-danger btn-sm m-2"
-        onClick={() => props.onDelete(props.id)}>
+        <button 
+        onClick={() => onDelete(id)}
+        className="btn btn-danger btn-sm m-2"
+        >
           Delete
          </button>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './app.jsx'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
-import Counters from './components/counters'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
